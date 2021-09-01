b1da2999ad0aded707d8241093a088d0af499fba
jeso20BTH <jeso20@student.bth.se>
2021-09-01 20:44:52 +0100

    Design updated for an neater look.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
+import './App.css';
+
 function Toolbar(props) {
 
     return (
-        <div>
-            {props.buttons.map(function(button, index) {
-                return Button(button, index)
-            })}
+        <div className="toolbar">
+            <div className="misc">
+                {props.buttons.map(function(button, index) {
+                    return Button(button, index)
+                })}
+            </div>
         </div>
     );
 }
 
 function Button(props, index) {
-    console.log(props);
     return (
         <div key={index} className="button" onClick={props.onClick}>
-            <p>{props.name}</p>
+            <i className="material-icons-outlined">{props.icon}</i>
+            <div className="button-text">
+                <p>{props.name}</p>
+            </div>
         </div>
     )
 }
@@ -38,10 +44,14 @@ export default function App() {
     ]
     return (
         <>
-            <Toolbar
-                buttons={buttons}
-            />
-            <Editor
+            <div className="header">
+                <span className="header-title">Editor of the people</span>
+            </div>
+            <div className="container">
+                <Toolbar
+                    buttons={buttons}
+                />
+                <Editor
                 onInit={(evt, editor) => editorRef.current = editor}
                 initialValue=""
                 apiKey="dcdovqi8pqzlmcoehtyvcyn4ofpg4050ojz2c0erbvk4ffas"
@@ -54,13 +64,16 @@ export default function App() {
                         'insertdatetime media table paste code help wordcount'
                     ],
                     toolbar: 'undo redo | formatselect | ' +
-                        'bold italic backcolor | alignleft aligncenter ' +
-                        'alignright alignjustify | bullist numlist outdent indent | ' +
-                        'removeformat | help',
+                    'bold italic backcolor | alignleft aligncenter ' +
+                    'alignright alignjustify | bullist numlist outdent indent | ' +
+                    'removeformat | help',
                     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                 }}
-            />
-            <button onClick={log}>Log editor content</button>
+                />
+            </div>
+            <div className="footer">
+                <p>© Copyright Jesper Stolt 2021</p>
+            </div>
         </>
     );
 }
